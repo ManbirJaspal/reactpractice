514fee0b88d8a116ead009e81b609f12a47b4509
Manbir Jaspal <manb.jaspal@gmail.com>
2019-03-06 13:54:12 -0500

    'StartedwithRedux'

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -8,6 +8,9 @@ class App extends React.Component {
     state = { videos: [],
               selectedVideo: null
             };
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    }
 
     onVideoSelect = (video) => {
         this.setState({selectedVideo: video});
@@ -21,14 +24,24 @@ class App extends React.Component {
         });
         
         console.log(response);
-        this.setState({ videos: response.data.items });
+        this.setState({ videos: response.data.items,
+                        selectedVideo: response.data.items[0]
+                      });
     };
     render() {
         return (
             <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            <div className="ui grid">
+            <div className="ui row">
+            <div className="eleven wide column">
             <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+            </div>
+            </div>
             </div>
                
                );
